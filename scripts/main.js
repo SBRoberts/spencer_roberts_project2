26c35efc4c0e4c751e60bf9a2ad280bc8a408e90
Spencer <spencerrca@gmail.com>
2018-08-05 00:43:54 -0400

    refined css and html

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        wrapAround: true,
+        autoPlay: true,
+        pageDots: false,
+        contain: true
+    });
+});
